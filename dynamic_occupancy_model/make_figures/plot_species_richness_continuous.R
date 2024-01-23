0f6fa00a573a8d1340f1d16b08a6e37a943bb5ae
jensculrich <jensj27@gmail.com>
2024-01-23 12:19:16 -0800

    finished richness polts (for year 1)

--- dynamic_occupancy_model/make_figures/plot_species_richness_continuous.R
+++ dynamic_occupancy_model/make_figures/plot_species_richness_continuous.R
@@ -61,186 +61,189 @@ occurrence1_simmed <- matrix(data = NA, nrow = n_species, ncol = pred_length)
 
 # expected values for a range of species and sites with range of herbaceous flowers
 # BUT with average woody flower
-for(i in 1:n_species){
+n_draws = 500
+random_draws_from_posterior = sample(1:nrow(list_of_draws), n_draws) 
+
+richness = matrix(nrow = pred_length, ncol = n_draws)
+
+for(draw in 1:n_draws){
+  
+  rand <- random_draws_from_posterior[draw]
+  
+  for(i in 1:n_species){
+    for(j in 1:pred_length){
+      occurrence1_expected[i,j] =
+        ilogit(#psi1_0 +
+          list_of_draws[rand,21] + 
+            #species_effects[species[i],1] + // a species specific intercept
+            # start at first row of species effects
+            # then each next species will be + 4 + i
+            list_of_draws[rand,(109+4*(i-1))] +
+            #psi1_herbaceous_flowers * herbaceous_flowers_scaled[j,1] + 
+            list_of_draws[rand,22] * pred_seq[j] + 
+            #psi1_specialization * d[i] +
+            list_of_draws[rand,24] * d[i] + 
+            #(psi1_interaction_1 * d[i] * herbaceous_flowers_scaled[j,1]) +
+            list_of_draws[rand,25] * d[i] * pred_seq[j] + 
+            #psi1_woody_flowers * woody_flowers_scaled[j,1] +
+            list_of_draws[rand,23] * 0 + 
+            #(psi1_interaction_2 * d[i] * woody_flowers_scaled[j,1])
+            list_of_draws[rand,26] * d[i] * 0 
+        )
+    }
+  }
+  
+  for(i in 1:n_species){
+    for(j in 1:pred_length){
+      occurrence1_simmed[i,j] <- rbinom(1, 1, prob = occurrence1_expected[i,j])
+    }
+  }
+  
   for(j in 1:pred_length){
-    occurrence1_expected[i,j] =
-      #psi1_0 +
-      fit_summary$summary[21,1] + 
-      #species_effects[species[i],1] + // a species specific intercept
-      # start at first row of species effects
-      # then each next species will be + 4 + i
-      fit_summary$summary[(109+4*(i-1)),1] + 
-      #psi1_herbaceous_flowers * herbaceous_flowers_scaled[j,1] + 
-      fit_summary$summary[22,1] * pred_seq[j] + 
-      #psi1_specialization * d[i] +
-      fit_summary$summary[24,1] * d[i] + 
-      #(psi1_interaction_1 * d[i] * herbaceous_flowers_scaled[j,1]) +
-      fit_summary$summary[25,1] * d[i] * pred_seq[j] + 
-      #psi1_woody_flowers * woody_flowers_scaled[j,1] +
-      fit_summary$summary[23,1] * 0+ 
-      #(psi1_interaction_2 * d[i] * woody_flowers_scaled[j,1])
-      fit_summary$summary[26,1] * d[i] * 0 
+    richness[j,draw] <- sum(occurrence1_simmed[1:n_species,j])
   }
 }
 
-# expected values for a range of species and sites with range of herbaceous flowers
-# BUT with average woody flower
+mean = vector(length=pred_length)
+lower_50 = vector(length=pred_length)
+upper_50 = vector(length=pred_length)
+lower_95 = vector(length=pred_length)
+upper_95 = vector(length=pred_length)
 
-randomd_draws_from_posterior = sample(1:nrow(list_of_draws), 100) 
-
-rand <- randomd_draws_from_posterior[1]
-
-for(i in 1:n_species){
-  for(j in 1:pred_length){
-    occurrence1_expected[i,j] =
-      ilogit(#psi1_0 +
-        list_of_draws[rand,21] + 
-        #species_effects[species[i],1] + // a species specific intercept
-        # start at first row of species effects
-        # then each next species will be + 4 + i
-        list_of_draws[rand,(109+4*(i-1))] +
-        #psi1_herbaceous_flowers * herbaceous_flowers_scaled[j,1] + 
-        list_of_draws[rand,22] * pred_seq[j] + 
-        #psi1_specialization * d[i] +
-        list_of_draws[rand,24] * d[i] + 
-        #(psi1_interaction_1 * d[i] * herbaceous_flowers_scaled[j,1]) +
-        list_of_draws[rand,25] * d[i] * pred_seq[j] + 
-        #psi1_woody_flowers * woody_flowers_scaled[j,1] +
-        list_of_draws[rand,23] * 0 + 
-        #(psi1_interaction_2 * d[i] * woody_flowers_scaled[j,1])
-        list_of_draws[rand,26] * d[i] * 0 
-    )
-  }
+for(i in 1:pred_length){
+  quants = as.vector(quantile(richness[i,], probs = c(0.05, 0.25, 0.50, 0.75, 0.95)))
+  
+  mean[i] = quants[3]
+  lower_50[i] = quants[2]
+  upper_50[i] = quants[4]
+  lower_95[i] = quants[1]
+  upper_95[i] = quants[5]
+  
 }
 
-for(i in 1:n_species){
-  for(j in 1:pred_length){
-    occurrence1_simmed[i,j] <- rbinom(1, 1, prob = occurrence1_expected[i,j])
-  }
-}
-
-richness = vector(length = pred_length)
-
-for(j in 1:pred_length){
-  richness[j] <- sum(occurrence1_simmed[1:n_species,j])
-}
-
-
-
-
-
-## --------------------------------------------------
-# set up for plot
-# parameter means
-X <- seq(1:n_years) # number of years
-
-# mean of eco params
-Y <- c(
-  fit_summary$summary[93,1], # mean species richness year 1
-  fit_summary$summary[94,1], # mean species richness year 2
-  fit_summary$summary[95,1] # mean species richness year 3
-)
-
-# confidence intervals
-lower_95 <- c(
-  fit_summary$summary[93,4], # mean species richness year 1
-  fit_summary$summary[94,4], # mean species richness year 2
-  fit_summary$summary[95,4] # mean species richness year 3
-)
-
-upper_95 <- c(
-  fit_summary$summary[93,8], # mean species richness year 1
-  fit_summary$summary[94,8], # mean species richness year 2
-  fit_summary$summary[95,8] # mean species richness year 3
-)
-
-lower_50 <- c(
-  fit_summary$summary[93,5], # mean species richness year 1
-  fit_summary$summary[94,5], # mean species richness year 2
-  fit_summary$summary[95,5] # mean species richness year 3
-)
-
-upper_50 <- c(
-  fit_summary$summary[93,7], # mean species richness year 1
-  fit_summary$summary[94,7], # mean species richness year 2
-  fit_summary$summary[95,7] # mean species richness year 3
-)
-
-df_estimates_enhanced <- as.data.frame(cbind(X, Y, 
-                                             lower_95, upper_95,
-                                             lower_50, upper_50)) %>%
-  mutate(habitat = "enhanced")
-
-# mean of eco params
-Y <- c(
-  fit_summary$summary[90,1], # mean species richness year 1
-  fit_summary$summary[91,1], # mean species richness year 2
-  fit_summary$summary[92,1] # mean species richness year 3
-)
-
-# confidence intervals
-lower_95 <- c(
-  fit_summary$summary[90,4], # mean species richness year 1
-  fit_summary$summary[91,4], # mean species richness year 2
-  fit_summary$summary[92,4] # mean species richness year 3
-)
-
-upper_95 <- c(
-  fit_summary$summary[90,8], # mean species richness year 1
-  fit_summary$summary[91,8], # mean species richness year 2
-  fit_summary$summary[92,8] # mean species richness year 3
-)
-
-lower_50 <- c(
-  fit_summary$summary[90,5], # mean species richness year 1
-  fit_summary$summary[91,5], # mean species richness year 2
-  fit_summary$summary[92,5] # mean species richness year 3
-)
-
-upper_50 <- c(
-  fit_summary$summary[90,7], # mean species richness year 1
-  fit_summary$summary[91,7], # mean species richness year 2
-  fit_summary$summary[92,7] # mean species richness year 3
-)
-
-df_estimates_control <- as.data.frame(cbind(X, Y, 
-                               lower_95, upper_95,
-                               lower_50, upper_50)) %>%
-  mutate(habitat = "control")
-
-df_estimates <- rbind(df_estimates_enhanced, df_estimates_control)
-
-df_estimates$X <- as.factor(df_estimates$X)
+df <- as.data.frame(cbind(pred_seq,
+                          mean,
+                          lower_50, upper_50,
+                          lower_95, upper_95))
 
 ## --------------------------------------------------
 ## Draw species richness plot
 
-(p <- ggplot(data = df_estimates, aes(X, Y, color = habitat)) +
-  scale_x_discrete(name="", breaks = c(1, 2, 3),
-                    labels=c("2021", "2022", "2023"
-                    )) +
-  geom_point(size = 8, position=position_dodge(width=0.5)) +
-  geom_errorbar(
-    aes(ymin = lower_95, ymax = upper_95),
-    width = 0.1,
-    position=position_dodge(width=0.5),
-    size=1) +
-   geom_errorbar(
-     aes(ymin = lower_50, ymax = upper_50),
-     width = 0,
-     position=position_dodge(width=0.5),
-     size=2.5) +
-  ylim(c(40, 85)) +
+(p <- ggplot(data = df, aes(pred_seq, mean)) +
+  geom_line(size=2) +
+  geom_ribbon(aes(ymin=lower_50, ymax=upper_50), fill = my_palette_reduced[1], alpha=0.8) + 
+  geom_ribbon(aes(ymin=lower_95, ymax=upper_95), fill = my_palette_reduced[1], alpha=0.1) + 
+  ylim(c(20, 85)) +
   theme_bw() +
-  ylab("species richness") +
+  ylab("predicted species richness (year 1)") +
+  xlab("herbaceous flower abundance") +
   scale_color_manual(values=my_palette_reduced) +
   theme(legend.text=element_text(size=10),
          axis.text.x = element_text(size = 18),
          axis.text.y = element_text(size = 18, angle=45, vjust=-0.5),
-         axis.title.x = element_blank(),
+         axis.title.x = element_text(size=20),
          axis.title.y = element_text(size = 20),
          panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
          panel.background = element_blank(), axis.line = element_line(colour = "black"))
 )
 
 
+##------------------------------------------------------------------------------
+## Now do it with woody plant richness
+
+## --------------------------------------------------
+## continuous habitat species richness plot
+
+pred_seq <- seq(-2, 2, 0.1)
+pred_length <- length(pred_seq)
+
+occurrence1_expected2 <- matrix(data = NA, nrow = n_species, ncol = pred_length)
+occurrence1_simmed2 <- matrix(data = NA, nrow = n_species, ncol = pred_length)
+
+# expected values for a range of species and sites with range of woody flowers
+# BUT with average woody flower
+n_draws = 500
+random_draws_from_posterior2 = sample(1:nrow(list_of_draws), n_draws) 
+
+richness2 = matrix(nrow = pred_length, ncol = n_draws)
+
+for(draw in 1:n_draws){
+  
+  rand <- random_draws_from_posterior2[draw]
+  
+  for(i in 1:n_species){
+    for(j in 1:pred_length){
+      occurrence1_expected2[i,j] =
+        ilogit(#psi1_0 +
+          list_of_draws[rand,21] + 
+            #species_effects[species[i],1] + // a species specific intercept
+            # start at first row of species effects
+            # then each next species will be + 4 + i
+            list_of_draws[rand,(109+4*(i-1))] +
+            #psi1_herbaceous_flowers * herbaceous_flowers_scaled[j,1] + 
+            list_of_draws[rand,22] * 0 + 
+            #psi1_specialization * d[i] +
+            list_of_draws[rand,24] * d[i] + 
+            #(psi1_interaction_1 * d[i] * herbaceous_flowers_scaled[j,1]) +
+            list_of_draws[rand,25] * d[i] * 0 + 
+            #psi1_woody_flowers * woody_flowers_scaled[j,1] +
+            list_of_draws[rand,23] * pred_seq[j] + 
+            #(psi1_interaction_2 * d[i] * woody_flowers_scaled[j,1])
+            list_of_draws[rand,26] * d[i] * pred_seq[j]
+        )
+    }
+  }
+  
+  for(i in 1:n_species){
+    for(j in 1:pred_length){
+      occurrence1_simmed2[i,j] <- rbinom(1, 1, prob = occurrence1_expected2[i,j])
+    }
+  }
+  
+  for(j in 1:pred_length){
+    richness2[j,draw] <- sum(occurrence1_simmed2[1:n_species,j])
+  }
+}
+
+mean2 = vector(length=pred_length)
+lower_50_2 = vector(length=pred_length)
+upper_50_2 = vector(length=pred_length)
+lower_95_2 = vector(length=pred_length)
+upper_95_2 = vector(length=pred_length)
+
+for(i in 1:pred_length){
+  quants = as.vector(quantile(richness2[i,], probs = c(0.05, 0.25, 0.50, 0.75, 0.95)))
+  
+  mean2[i] = quants[3]
+  lower_50_2[i] = quants[2]
+  upper_50_2[i] = quants[4]
+  lower_95_2[i] = quants[1]
+  upper_95_2[i] = quants[5]
+  
+}
+
+df2 <- as.data.frame(cbind(pred_seq,
+                          mean2,
+                          lower_50_2, upper_50_2,
+                          lower_95_2, upper_95_2))
+
+## --------------------------------------------------
+## Draw species richness plot
+
+(p <- ggplot(data = df2, aes(pred_seq, mean2)) +
+   geom_line(size=2) +
+   geom_ribbon(aes(ymin=lower_50_2, ymax=upper_50_2), fill = my_palette_reduced[2], alpha=0.8) + 
+   geom_ribbon(aes(ymin=lower_95_2, ymax=upper_95_2), fill = my_palette_reduced[2], alpha=0.1) + 
+   ylim(c(20, 85)) +
+   theme_bw() +
+   ylab("predicted species richness (year 1)") +
+   xlab("woody flower abundance") +
+   scale_color_manual(values=my_palette_reduced) +
+   theme(legend.text=element_text(size=10),
+         axis.text.x = element_text(size = 18),
+         axis.text.y = element_text(size = 18, angle=45, vjust=-0.5),
+         axis.title.x = element_text(size = 20),
+         axis.title.y = element_text(size = 20),
+         panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+         panel.background = element_blank(), axis.line = element_line(colour = "black"))
+)
